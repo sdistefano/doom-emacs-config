8145eee59100ecd4dfe66e65fa9c1eb4619ac6e3
sdistefano <sdistefano@gmail.com>
2022-01-24 12:07:00 +0000

    config.el

--- config.el
+++ config.el
@@ -176,7 +176,7 @@
         (defun google-translate-at-point-gl()
           ""
           (interactive)
-          (cond ((eq (var google-translate-default-target-language) "ja") (google-translate-at-point-jp))
+          (cond ((eq google-translate-default-target-language "ja") (google-translate-at-point-jp))
           (t (google-translate-at-point-nr))
           )
         )
